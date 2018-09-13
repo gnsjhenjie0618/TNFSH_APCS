e374bf11afa9c6ea7279e68101760efc502af119
gnsJhenJie <32983347+gnsJhenJie@users.noreply.github.com>
2018-09-13 22:15:22 +0800

    Create UVa484.cpp

---
+++ W2_UVa484/UVa484.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100000]={};
+    int b[100000]={};
+    int tmp;
+    int now=0;
+    bool in=false;
+    while (cin>>tmp){
+        for (int i=0;i<now;i++){
+            if (a[i]==tmp){
+                b[a[i]]++;
+                in=true;
+            }
+        }
+        if (!in) {
+            a[now]=tmp;
+            b[a[now]]++;
+            now++;
+        }
+        in = false;
+    }
+    for (int i=0;i<now;i++){
+        cout << a[i] << ' ' << b[a[i]] << '\n';
+    }
+
+    return 0;
+}
